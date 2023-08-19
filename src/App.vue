bd44b15f8806e6659f0fe3444a26c56633b0baa0
lxchapu <lxchapu@outlook.com>
2023-08-19 12:18:14 +0800

    feat: optimize display for mobile devices

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,28 @@
 <script setup lang="ts">
+import { onMounted } from 'vue'
 import { RouterView } from 'vue-router'
+
+/**
+ * 判断设备是否为移动端
+ */
+function isMobile() {
+  const userAgentInfo = navigator.userAgent
+  const agents = ['Android', 'iPhone', 'SymbianOS', 'Windows Phone', 'iPad', 'iPod']
+  let flag = false
+  for (let v = 0; v < agents.length; v++) {
+    if (userAgentInfo.indexOf(agents[v]) > 0) {
+      flag = true
+      break
+    }
+  }
+  return flag
+}
+
+onMounted(() => {
+  if (isMobile()) {
+    document.body.style.zoom = window.innerWidth / 1500 + ''
+  }
+})
 </script>
 
 <template>
