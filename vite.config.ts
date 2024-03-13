5ce06c178e6b5c318e9000b1bc6d6b1ced6b0c98
lxchapu <lxchapu@outlook.com>
2024-03-13 19:56:22 +0800

    fix: only add tracking code in production

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,17 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { resolve } from 'path'
 import UnoCSS from 'unocss/vite'
+import { createHtmlPlugin } from 'vite-plugin-html'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), UnoCSS(),],
+  plugins: [
+    vue(),
+    UnoCSS(),
+    createHtmlPlugin({
+      entry: 'src/main.ts',
+    }),
+  ],
   resolve: {
     alias: {
       '@': resolve(__dirname, 'src'),
